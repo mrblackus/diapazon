c54d06d2ecda7dffa18d3c40d7f0e0ca24466b57
Mathieu Savy <mathieu.savy@gmail.com>
2014-03-19 14:28:40 +0100

    Add bootstrap and basic durandal modules

--- public/app/shell.js
+++ public/app/shell.js
@@ -1,15 +1,14 @@
-/**
- * Created by mathieu.savy on 3/19/14.
- */
-
 define(function (require) {
-    var app = require('durandal/app'),
-        ko = require('knockout');
+    var router = require('plugins/router');
 
     return {
-        name: ko.observable(),
-        sayHello: function() {
-            app.showMessage('Hello ' + this.name() + '! Nice to meet you.', 'Greetings');
+        router: router,
+        activate: function () {
+            router.map([
+                { route: '', title:'Home', moduleId: 'home', nav: true }
+            ]).buildNavigationModel();
+
+            return router.activate();
         }
     };
 });
